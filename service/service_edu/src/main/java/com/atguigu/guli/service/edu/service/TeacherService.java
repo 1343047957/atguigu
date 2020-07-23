42e3f1b0e68895987b194847311f18e4efb8dbe9
yaozefeng <1343047957@qq.com>
2020-07-23 18:25:28 +0800

    第一次

---
+++ service/service_edu/src/main/java/com/atguigu/guli/service/edu/service/TeacherService.java
@@ -0,0 +1,26 @@
+package com.atguigu.guli.service.edu.service;
+
+import com.atguigu.guli.service.edu.entity.Teacher;
+import com.atguigu.guli.service.edu.entity.vo.TeacherQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author Helen
+ * @since 2020-04-01
+ */
+public interface TeacherService extends IService<Teacher> {
+
+    IPage<Teacher> selectPage(Page<Teacher> pageParam, TeacherQueryVo teacherQueryVo);
+
+
+//    void selectMapsPage(Page<Teacher> objectPage, QueryWrapper<Teacher> queryWrapperTeacher);
+}
